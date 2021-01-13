96554ff1faf1a85eaf5f35ff8fabfafea46f8184
Ulrich Berthold <u@sansculotte.net>
2021-01-13 23:21:35 +0100

    nicer style for task list

--- webapp-js/components/TaskList.tsx
+++ webapp-js/components/TaskList.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 
 import Task from 'interfaces/Task'
 
+import * as style from './TaskList.scss'
+
 
 interface Props {
   tasks: Map<string, Task>
@@ -10,9 +12,12 @@ interface Props {
 
 export default (props: Props) =>
   <section>
-    <ul>
+    <ul className={style.taskList}>
       {Array.from(props.tasks.values()).map(t =>
-        <li key={t.uuid}>{ t.name }{ t.status === 'done' && <a href={t.url}>result</a>}</li>)
-      }
+        <li key={t.uuid}>
+          <dt>{ t.name }</dt>
+          <dd>{ t.status === 'done' && <a href={t.url}>result</a>}</dd>
+        </li>
+      )}
     </ul>
   </section>
